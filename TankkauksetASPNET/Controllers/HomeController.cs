8cf2617760287830d6a272fce6acf191674fbc6e
Mika Koivisto <mika.koivisto@student.careeria.fi>
2022-07-22 16:21:31 +0300

    Login works + navbar contents changed

--- TankkauksetASPNET/Controllers/HomeController.cs
+++ TankkauksetASPNET/Controllers/HomeController.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TankkauksetASPNET.Models;
 
 namespace TankkauksetASPNET.Controllers
 {
     public class HomeController : Controller
     {
+        private TankkauksetEntities db = new TankkauksetEntities(); 
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.Username == LoginModel.Username && x.Password == LoginModel.Password);
+            if (LoggedUser != null)
+            {
+                //ViewBag.LoginMessage = "Kirjautuminen onnistui!";
+                ViewBag.LoggedStatus = "In";
+                ViewBag.LoginError = 0; //Ei virhettä
+                Session["Username"] = LoggedUser.Username;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Kirjautuminen epäonnistui.";
+                ViewBag.LoggedStatus = "Out";
+                ViewBag.LoginError = 1; //Pakotetaan modaali login-ruutu uudelleen, koska kirjautumisyritys epäonnistui
+                return View("ReDirect", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivulle
+        }
         public ActionResult Index()
         {
             return View();

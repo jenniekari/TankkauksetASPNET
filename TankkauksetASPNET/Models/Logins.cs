9ad4ecf8d7237a7043e2c86a0f84536240cde3a7
Mika Koivisto <mika.koivisto@student.careeria.fi>
2022-08-23 15:20:56 +0300

    Added password masking back

--- TankkauksetASPNET/Models/Logins.cs
+++ TankkauksetASPNET/Models/Logins.cs
@@ -11,11 +11,15 @@ namespace TankkauksetASPNET.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Logins
     {
         public int LoginId { get; set; }
+        [Required(ErrorMessage = "Anna käyttäjätunnus!")]
         public string Username { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Anna salasana!")]
         public string Password { get; set; }
         public string ErrorMessage { get; set; }
     }
